63e417e8dc57412125cd7bd2d19f2780ca8383be
@mobolade <@mobolade.akinlua@outlook.com>
2022-04-12 18:34:05 +0100

    feat:nav bar component

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react'
 import reportWebVitals from './reportWebVitals';
+import { extendTheme } from '@chakra-ui/react'
+import '@fontsource/raleway/400.css'
 
-ReactDOM.render(
-  <ChakraProvider>
-    <App />
-  </ChakraProvider>,
-  document.getElementById('root')
+const theme = extendTheme({
+  fonts: {
+    heading: 'Raleway, sans-serif',
+    body: 'Raleway, sans-serif',
+  },
+})
+
+export default theme
+
+
+const container = document.getElementById('root') as HTMLElement;
+const root = createRoot(container);
+root.render(
+<ChakraProvider theme={theme}>
+  <App />
+</ChakraProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
