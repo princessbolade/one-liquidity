f4a062fcfa2bf28ed7622844f95aed02fa2ebb82
@mobolade <@mobolade.akinlua@outlook.com>
2022-04-13 12:26:12 +0100

    about-us component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {Box, Text } from '@chakra-ui/react';
 import Nav from './components/nav-bar';
 import Hero from './components/hero';
 import GetStarted from './components/get-started';
+import AboutUs from './components/about-us';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
      <Nav/>
      <Hero/>
      <GetStarted/>
+     <AboutUs/>
    </Box>
   );
 }
---
+++ src/components/about-us/index.tsx
@@ -0,0 +1,17 @@
+import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+import React from "react";
+
+function AboutUs() {
+  return (
+    <Box mx={"auto"} maxW={"1440px"} py={"50px"} px={"20px"}>
+      <Heading textAlign={"center"}>About us</Heading>
+      <Flex>
+        <Box>
+        <Text>hahaha</Text>
+        </Box>
+      </Flex>
+    </Box>
+  );
+}
+
+export default AboutUs;
